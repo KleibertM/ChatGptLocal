b84ffd4ab8f7033f34466e5960612cec29254f42
kleibert medina <kleibertmedina@gmail.com>
2024-08-20 22:50:17 -0500

    Falta mejorar la velocidad de carga en la respuesta del bot, y mejorar la UI

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { CreateWebWorkerMLCEngine } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm"
+
 const $ = el => document.querySelector(el)
 
 // pongo delante de la variable un símbolo de $
@@ -8,8 +10,32 @@ const $template = $('#message-template')
 const $messages = $('ul')
 const $container = $('main')
 const $button = $('button')
+const $info = $('small')
+const $loading = $('.loading')
 
-$form.addEventListener('submit', (e) => {
+let messages = []
+let end = false
+
+const SELECTED_MODEL = 'Llama-3-8B-Instruct-q4f32_1-MLC-1k'
+
+const engine = await CreateWebWorkerMLCEngine(
+    new Worker('./worker.js', { type: 'module' }),
+    SELECTED_MODEL,
+    {
+        initProgressCallback: (info) => {
+            $info.textContent = info.text
+            if (info.progress === 1 && !end) {
+                end = true
+                $loading?.parentNode.removeChild($loading)
+                $button.removeAttribute('disabled')
+                addMessage('Hola! Sou un ChatGPT que se ejecuta en el navegador')
+                $input.focus()
+            }
+        }
+    }
+)
+
+$form.addEventListener('submit', async (e) => {
     e.preventDefault()
     const messageText = $input.value.trim()
 
@@ -18,10 +44,42 @@ $form.addEventListener('submit', (e) => {
     }
 
     addMessage(messageText, 'user')
+    $button.setAttribute('disabled', '')
+
+    const userMessage = {
+        role: 'user',
+        content: messageText
+    }
+
+    messages.push(userMessage)
+
+    const chunks = await engine.chat.completions.create({
+        messages,
+        stream: true
+    })
+
+    let reply = ''
+
+    const $botMessage = addMessage("", 'bot')
+
+    for await (const chunk of chunks) {
+        const choice = chunk.choices[0]
+        const content = choice?.delta?.content ?? ""
+        reply += content
+        $botMessage.textContent = reply
+    }
+
+    $button.removeAttribute('disabled')
+    messages.push({
+        role: 'assistant',
+        content: reply
+    })
+    $container.scrollTop = $container.scrollHeight
 })
 
+
 function addMessage(text, sender) {
-    // clonar el template
+    //clonar el template
     const clonedTemplate = $template.content.cloneNode(true)
     const $newMessage = clonedTemplate.querySelector('.message')
 
@@ -37,4 +95,4 @@ function addMessage(text, sender) {
     $container.scrollTop = $container.scrollHeight
 
     return $text
-  }
+}
